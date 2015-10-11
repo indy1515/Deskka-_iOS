4afff40dd85e39486186583b598d4eb379ac41bb
indy1515 <indy1_thailand@hotmail.com>
2015-10-12 04:47:07 +0700

    Try adding camera

---
+++ Deskka/Deskka/ScanViewController.h
@@ -0,0 +1,38 @@
+//
+//  ScanViewController.h
+//  Deskka
+//
+//  Created by IndyZa on 10/12/2558 BE.
+//  Copyright © 2558 IndyZaLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@protocol ScanViewControllerDelegate;
+
+@interface ScanViewController : UIViewController <AVCaptureMetadataOutputObjectsDelegate>
+
+@property (weak, nonatomic) IBOutlet UIView *contentView;
+
+@property (weak, nonatomic) IBOutlet UILabel *cancelButton;
+
+@property (nonatomic, weak) id<ScanViewControllerDelegate> delegate;
+
+@property (assign, nonatomic) BOOL touchToFocusEnabled;
+
+- (BOOL) isCameraAvailable;
+- (void) startScanning;
+- (void) stopScanning;
+- (void) setTorch:(BOOL) aStatus;
+
+@end
+
+@protocol ScanViewControllerDelegate <NSObject>
+
+@optional
+
+- (void) scanViewController:(ScanViewController *) aCtler didTapToFocusOnPoint:(CGPoint) aPoint;
+- (void) scanViewController:(ScanViewController *) aCtler didSuccessfullyScan:(NSString *) aScannedValue;
+
+@end
---
+++ Deskka/Deskka/ScanViewController.m
@@ -0,0 +1,188 @@
+//
+//  ScanViewController.m
+//  Deskka
+//
+//  Created by IndyZa on 10/12/2558 BE.
+//  Copyright © 2558 IndyZaLab. All rights reserved.
+//
+
+#import "ScanViewController.h"
+
+@interface ScanViewController ()
+
+@property (strong, nonatomic) AVCaptureDevice* device;
+@property (strong, nonatomic) AVCaptureDeviceInput* input;
+@property (strong, nonatomic) AVCaptureMetadataOutput* output;
+@property (strong, nonatomic) AVCaptureSession* session;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer* preview;
+
+@end
+
+@implementation ScanViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.contentView setNeedsLayout];
+    // Do any additional setup after loading the view.
+    if([self isCameraAvailable]) {
+        [self setupScanner];
+    }
+    [self startScanning];
+}
+
+- (void)viewWillAppear:(BOOL)animated;
+{
+    [super viewWillAppear:animated];
+    if(![self isCameraAvailable]) {
+        [self setupNoCameraView];
+    }
+}
+
+- (void) viewDidAppear:(BOOL)animated;
+{
+    [super viewDidAppear:animated];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)touchesBegan:(NSSet*)touches withEvent:(UIEvent*)evt
+{
+    NSLog(@"Touch ");
+    if(self.touchToFocusEnabled) {
+        UITouch *touch=[touches anyObject];
+        CGPoint pt= [touch locationInView:self.view];
+        [self focus:pt];
+    }
+}
+
+#pragma mark -
+#pragma mark AVFoundationSetup
+
+- (void) setupScanner;
+{
+    self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    self.input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
+    
+    self.session = [[AVCaptureSession alloc] init];
+    
+    self.output = [[AVCaptureMetadataOutput alloc] init];
+    [self.session addOutput:self.output];
+    [self.session addInput:self.input];
+    
+    [self.output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    self.output.metadataObjectTypes = @[AVMetadataObjectTypeQRCode];
+    
+    self.preview = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    self.preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    self.preview.frame = CGRectMake(0, 0
+                                    , self.contentView.frame.size.width, self.contentView.frame.size.height);
+    NSLog(@"ContentView: %fx%f",self.contentView.frame.size.width, self.contentView.frame.size.height);
+    
+    AVCaptureConnection *con = self.preview.connection;
+    
+    con.videoOrientation = AVCaptureVideoOrientationPortrait;
+    
+    [self.view.layer insertSublayer:self.preview atIndex:1];
+}
+
+#pragma mark -
+#pragma mark NoCamAvailable
+
+- (void) setupNoCameraView;
+{
+    UILabel *labelNoCam = [[UILabel alloc] init];
+    labelNoCam.text = @"No Camera available";
+    labelNoCam.textColor = [UIColor whiteColor];
+    [self.view addSubview:labelNoCam];
+    [labelNoCam sizeToFit];
+    labelNoCam.center = self.view.center;
+}
+
+
+#pragma mark -
+#pragma mark Helper Methods
+
+- (BOOL) isCameraAvailable;
+{
+    NSArray *videoDevices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    return [videoDevices count] > 0;
+}
+
+- (void)startScanning;
+{
+    [self.session startRunning];
+    
+}
+
+- (void) stopScanning;
+{
+    [self.session stopRunning];
+}
+
+- (void) setTorch:(BOOL) aStatus;
+{
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    [device lockForConfiguration:nil];
+    if ( [device hasTorch] ) {
+        if ( aStatus ) {
+            [device setTorchMode:AVCaptureTorchModeOn];
+        } else {
+            [device setTorchMode:AVCaptureTorchModeOff];
+        }
+    }
+    [device unlockForConfiguration];
+}
+
+- (void) focus:(CGPoint) aPoint;
+{
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if([device isFocusPointOfInterestSupported] &&
+       [device isFocusModeSupported:AVCaptureFocusModeAutoFocus]) {
+        CGRect screenRect = [[UIScreen mainScreen] bounds];
+        double screenWidth = screenRect.size.width;
+        double screenHeight = screenRect.size.height;
+        double focus_x = aPoint.x/screenWidth;
+        double focus_y = aPoint.y/screenHeight;
+        if([device lockForConfiguration:nil]) {
+            if([self.delegate respondsToSelector:@selector(scanViewController:didTapToFocusOnPoint:)]) {
+                [self.delegate scanViewController:self didTapToFocusOnPoint:aPoint];
+            }
+            [device setFocusPointOfInterest:CGPointMake(focus_x,focus_y)];
+            [device setFocusMode:AVCaptureFocusModeAutoFocus];
+            if ([device isExposureModeSupported:AVCaptureExposureModeAutoExpose]){
+                [device setExposureMode:AVCaptureExposureModeAutoExpose];
+            }
+            [device unlockForConfiguration];
+        }
+    }
+}
+
+#pragma mark -
+#pragma mark AVCaptureMetadataOutputObjectsDelegate
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects
+       fromConnection:(AVCaptureConnection *)connection
+{
+    for(AVMetadataObject *current in metadataObjects) {
+        if([current isKindOfClass:[AVMetadataMachineReadableCodeObject class]]) {
+            if([self.delegate respondsToSelector:@selector(scanViewController:didSuccessfullyScan:)]) {
+                NSString *scannedValue = [((AVMetadataMachineReadableCodeObject *) current) stringValue];
+                [self.delegate scanViewController:self didSuccessfullyScan:scannedValue];
+            }
+        }
+    }
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
